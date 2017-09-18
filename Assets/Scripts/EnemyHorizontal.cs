7537b08d15b53d024a048348a27f47fd55cd2308
Alexandra Lu <cml694@nyu.edu>
2017-09-18 09:34:02 -0400

    CameraFollow/Sight added (but still broken rip)

--- Assets/Scripts/EnemyHorizontal.cs
+++ Assets/Scripts/EnemyHorizontal.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHorizontal : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private bool dirRight = true;
+    public float speed = 2.0f;
+    private SpriteRenderer mySpriteRenderer;
+    // Use this for initialization
+    void Start () {
+     //   mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (dirRight)
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        else
+            transform.Translate(-Vector2.right * speed * Time.deltaTime);
+
+        if (transform.position.x >= 0.5f)
+        {
+            dirRight = false;
+        //    mySpriteRenderer.flipX = true;
+        }
+
+        if (transform.position.x <= -6)
+        {
+            dirRight = true;
+           // mySpriteRenderer.flipX = false;
+        }
+    }
 }
