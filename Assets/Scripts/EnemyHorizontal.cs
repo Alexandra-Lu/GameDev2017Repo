0d81770366af9714700cb34f12a4765df599b935
Alexandra Lu <cml694@nyu.edu>
2017-09-20 11:44:07 -0400

    Started improving level design

--- Assets/Scripts/EnemyHorizontal.cs
+++ Assets/Scripts/EnemyHorizontal.cs
@@ -18,16 +18,27 @@ public class EnemyHorizontal : MonoBehaviour {
         else
             transform.Translate(-Vector2.right * speed * Time.deltaTime);
 
-        if (transform.position.x >= 0.5f)
+        if (transform.position.x >= 12f)
         {
             dirRight = false;
         //    mySpriteRenderer.flipX = true;
         }
 
-        if (transform.position.x <= -6)
+        if (transform.position.x <= -2)
+            //!!!!!All enemies under this script patrol the same x-coordinates regardless of where
+            //!!!!!they are initially placed. This needs to be changed to a distance range so the same 
+            //!!!!!enemy can be placed around the house without being drawn to the same 'column'
         {
             dirRight = true;
            // mySpriteRenderer.flipX = false;
         }
     }
 }
+
+
+
+//OTHER ENEMIES
+// AAA One rotation enemy; single sight. spins gradually; bottom right corner
+// BBB Enemy that patrols, stops to spin around, turns back and contines down the path
+// CCC Every x seconds, a dog that is otherwise sleeping runs out and follows a specific path
+//     around the house, audio cue of barking (any way to place specific points/coordinates?)
